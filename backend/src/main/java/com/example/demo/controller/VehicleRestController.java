e2ccc6c45e09847aac4b5c70c9dbd81017b17d09
Michal Vaniš <zirixcz@gmail.com>
2023-03-19 18:10:30 +0100

    CORS

--- backend/src/main/java/com/example/demo/controller/VehicleRestController.java
+++ backend/src/main/java/com/example/demo/controller/VehicleRestController.java
@@ -5,12 +5,12 @@ import com.example.demo.service.VehicleService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+
+@CrossOrigin(origins = "*")
 
 @RestController
 public class VehicleRestController {
