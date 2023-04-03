659f70dc9a760f9cc92c7843feb6bd6c22179c6b
Michal Vaniš <zirixcz@gmail.com>
2023-04-03 22:32:49 +0200

    add radio buttons & refactor

--- frontend/index.js
+++ frontend/index.js
@@ -1,65 +1,120 @@
+// Hook vehicleList<table> & fetchBtn<button>
 let vehicleList = document.getElementById("vehicleList");
 let fetchBtn = document.getElementById("fetch");
 
-// submit
-let submitBtn = document.getElementById("addVehicleBtn");
-let uic = document.getElementById("uicInput");
-let length = document.getElementById("lengthInput");
-let maxspeed = document.getElementById("speedInput");
-let locomotiveType = document.getElementById("locomotiveType");
-let power = document.getElementById("power");
-let pullPower = document.getElementById("pullPower");
-let capacity = document.getElementById("capacity");
-let maxCarryWeight = document.getElementById("maxCarryWeight");
-
-let fileUpload = document.getElementById("fileInput");
-
-submitBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  const vehicle = [
-    {
-      uic: uic.value,
-      length: length.value,
-      maxspeed: maxspeed.value,
-      locomotiveType: locomotiveType.value,
-      power: power.value,
-      pullPower: pullPower.value,
-      capacity: capacity.value,
-      maxCarryWeight: maxCarryWeight.value,
-    },
-  ];
-  addVehicle(vehicle);
-});
-
-fileUpload.addEventListener("change", function (event) {
-  var reader = new FileReader();
-
-  reader.onload = function (event) {
-    var jsonObj = JSON.parse(event.target.result);
-    addVehicle(jsonObj);
-  };
-
-  reader.readAsText(event.target.files[0]);
-});
-
-function addVehicle(vehicle) {
-  if (vehicle.length === 0) return;
-  // check of global properties
-  for (let i = 0; i < vehicle.length; i++) {
-    if (!vehicle[i].uic || !vehicle[i].length || !vehicle[i].maxspeed) return;
-  }
-
-  fetch("http://127.0.0.1:8080/vehicle", {
+const callApi = (vehicle, type) =>
+  fetch(`http://127.0.0.1:8080/add${type}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(vehicle),
+    body: vehicle,
   })
     .then((response) => response.json())
     .then((data) => {
       console.log("Success:", data);
     });
+
+// Fetch values from HTML inputs
+const getValues = () => [
+  {
+    uic: document.getElementById("uicInput").value,
+    length: document.getElementById("lengthInput").value,
+    maxspeed: document.getElementById("speedInput").value,
+    locomotiveType: document.getElementById("locomotiveType").value,
+    power: document.getElementById("power").value,
+    pullPower: document.getElementById("pullPower").value,
+    capacity: document.getElementById("capacity").value,
+    maxCarryWeight: document.getElementById("maxCarryWeight").value,
+  },
+];
+
+const type = {
+  locomotive: false,
+  cargo: false,
+  wagon: false,
+};
+document.querySelectorAll('input[type="radio"]').forEach((button, i) => {
+  button.addEventListener("change", () => {
+    if (button.id === "locomotiveRadio") {
+      type.locomotive = true;
+      type.cargo = false;
+      type.wagon = false;
+    }
+    if (button.id === "cargoRadio") {
+      type.locomotive = false;
+      type.cargo = true;
+      type.wagon = false;
+    }
+    if (button.id === "wagonRadio") {
+      type.locomotive = false;
+      type.cargo = false;
+      type.wagon = true;
+    }
+  });
+});
+
+// Add a new vehicle on click
+document
+  .getElementById("addVehicleBtn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    addVehicle(getValues());
+  });
+
+// Add a new vehicle on file upload
+document
+  .getElementById("fileInput")
+  .addEventListener("change", function (event) {
+    var reader = new FileReader();
+
+    reader.onload = function (event) {
+      var jsonObj = JSON.parse(event.target.result);
+      addVehicle(jsonObj);
+    };
+
+    reader.readAsText(event.target.files[0]);
+  });
+
+// Add vehicle function
+function addVehicle(vehicle) {
+  // return if there is no vehicle to be added
+  if (vehicle.length === 0) return;
+
+  // return if there is no uic, length or maxspeed
+  for (let i = 0; i < vehicle.length; i++) {
+    if (!vehicle[i].uic || !vehicle[i].length || !vehicle[i].maxspeed) return;
+  }
+
+  if (type.locomotive) {
+    for (let i = 0; i < vehicle.length; i++) {
+      if (
+        !vehicle[i].locomotiveType ||
+        !vehicle[i].power ||
+        !vehicle[i].pullPower
+      ) {
+        alert("invalid locomotive");
+        return;
+      }
+    }
+    callApi(JSON.stringify(vehicle), "Locomotive");
+  } else if (type.cargo) {
+    for (let i = 0; i < vehicle.length; i++) {
+      if (!vehicle[i].maxCarryWeight) {
+        alert("invalid cargo");
+        return;
+      }
+    }
+    callApi(JSON.stringify(vehicle), "Cargo");
+  } else if (type.wagon) {
+    for (let i = 0; i < vehicle.length; i++) {
+      if (!vehicle[i].capacity) {
+        alert("invalid wagon");
+        return;
+      }
+    }
+    callApi(JSON.stringify(vehicle), "Wagon");
+  }
 }
 
 let vehicles = [];
